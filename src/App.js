10b7e64e8cea9da3685620d046b55e7dd2ab1059
João Neves <13joaoneves@gmail.com>
2019-12-13 14:27:04 +0100

    Initialize client app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  stream = new EventSource('http://localhost:4000/stream')
+
+  componentDidMount() {
+    this.stream.onmessage = event => {
+      console.log('event.data test:', event.data);
+      
+      const parsed = JSON.parse(event.data)
+      console.log('parsed test', parsed);
+      
+    }
+  }
+  render () {
+    return <div>Client</div>
+  }
 }
-
 export default App;
