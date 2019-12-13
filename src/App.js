24ff0d9d5c01b5d9ffab77eec8b6a85360b929bd
João Neves <13joaoneves@gmail.com>
2019-12-13 17:17:23 +0100

    add submit form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import superagent from 'superagent'
 
 // 1. Remove allMessages import
 // 2. Remove mapDispatchToProps object
@@ -7,6 +8,10 @@ import { connect } from 'react-redux'
 // 4. Dispatch action directly from componenetDidMount wiht this.props.dispatch
 
 class App extends Component {
+  state = {
+    text: ''
+  }
+
   stream = new EventSource('http://localhost:4000/stream')
 
   componentDidMount() {
@@ -20,6 +25,36 @@ class App extends Component {
       
     }
   }
+
+  onChange = (event) => {
+    // const value = event.target.value
+    // OR 
+      const { target: { value } } = event
+    // target === undefined
+    // value === 'some string value'
+
+    this.setState( { text: value } )
+  }
+
+  reset = () => {
+    this.setState( { text: '' } )
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault()
+
+    const url = 'http://localhost:4000/message'
+
+    const response = await superagent
+    .post(url)
+    .send(this.state)
+
+    this.reset()
+
+    console.log('response test:', response);
+  
+  }
+
   render () {
     console.log('this.props test:', this.props);
     
@@ -28,14 +63,26 @@ class App extends Component {
     const list = messages.map(message => 
       <p key={message.id}>{message.text}</p>)
 
-    return <div>{list}</div>
+    return <main>
+      <form onSubmit={this.onSubmit}>
+        <input 
+        onChange={this.onChange} 
+        type='text'
+        value={this.state.text}/>
+        <button>Submit</button>
+      </form>
+
+      <button onClick={this.reset}>Reset</button>
+
+      {list}
+      </main>
   }
 }
 
 
-//get data from store
+  //Get data from store
 function mapStateToProps (state) {
-  //state is the current data in the redux store
+  //State is the current data in the redux store
   //Each property of the object becomes a props of the component 
   return {
     messages: state // Inside of the component, this.props.messages will be the entire state of the redux store
